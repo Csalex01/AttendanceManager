9e02ec2ebbaa3bd614f16c871c450ee94b9a7db1
Csalex01 <csillagalex2001@gmail.com>
2022-10-27 13:55:41 +0300

    Initialized database and created test script (with @Simonhunor5)

---
+++ src/AttendanceManager/models.py
@@ -0,0 +1,33 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Users(db.Model, UserMixin):
+  NeptunCode = db.Column(db.String(6), primary_key=True)
+  Email = db.Column(db.String(100), unique=True, nullable=False)
+  Password = db.Column(db.String(100), unique=True, nullable=False)
+  FirstName = db.Column(db.String(50), unique=True, nullable=False)
+  LastName = db.Column(db.String(50), unique=True, nullable=False)
+  UserType = db.Column(db.Boolean, default=False)
+
+class Courses(db.Model):
+  CourseID = db.Column(db.Integer, primary_key=True)
+  TeacherCode = db.Column(db.Integer, unique=True, nullable=False)
+  Name = db.Column(db.String(50), unique=True, nullable=False)
+  Department = db.Column(db.Integer, nullable=False)
+
+class EnrolledStudents(db.Model):
+  StudentCode = db.Column(db.Integer, primary_key=True)
+  CourseID = db.Column(db.Integer, primary_key=True)
+  Approved = db.Column(db.Boolean, default=False)
+
+class CourseDates(db.Model):
+  OccasionID = db.Column(db.Integer, primary_key=True)
+  CourseID = db.Column(db.Integer, unique=True, nullable=False)
+  Type = db.Column(db.Integer, nullable=False)
+  Classroom = db.Column(db.Integer, nullable=False)
+
+class Attendance(db.Model):
+  CourseID = db.Column(db.Integer, primary_key=True)
+  StudentCode = db.Column(db.Integer, primary_key=True)
+  Present = db.Column(db.Boolean, default=False)
