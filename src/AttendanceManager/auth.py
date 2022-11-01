a6a6771b772c4bec63b4a3d3c9aca570d90acae2
Csalex01 <csillagalex2001@gmail.com>
2022-11-01 13:35:21 +0200

    User Authentication

--- src/AttendanceManager/auth.py
+++ src/AttendanceManager/auth.py
@@ -1,4 +1,8 @@
+from . import db
+from .models import Users, Attendance, CourseDates, Courses, EnrolledStudents, Teachers
+
 from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask_login import current_user, login_user, logout_user
 
 # Initialize blueprint
 auth = Blueprint("auth", __name__)
@@ -14,6 +18,12 @@ def signup():
       last_name = request.form.get("last_name")
       password = request.form.get("password")
       confirm_password = request.form.get("confirm_password")
+      user_type = 0
+      
+      teacher = Teachers.query.filter_by(NeptunCode=neptun_code).first()
+
+      if teacher != None:
+        user_type = 1
 
       print(f"Email: {email}")
       print(f"Neptun Code: {neptun_code}")
@@ -21,15 +31,49 @@ def signup():
       print(f"Last Name: {last_name}")
       print(f"Password: {password}")
       print(f"Confirm Password: {confirm_password}")
+      print(f"User Type: {user_type}")
 
       # TODO Check for input validity (length, format, etc...)
 
-  return render_template("auth/signup.html")
+      new_user = Users(
+        NeptunCode=neptun_code,
+        Email=email,
+        Password=password,
+        FirstName=first_name,
+        LastName=last_name,
+        UserType=user_type
+      )
 
-@auth.route("/login")
+      db.session.add(new_user)
+      db.session.commit()
+
+      login_user(new_user, remember=True)
+
+      return redirect(url_for("general.index"))
+
+  else:
+    return render_template("auth/signup.html")
+
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-  return render_template("auth/login.html")
+  if request.method == "POST":
+    email = request.form.get("email")
+    password = request.form.get("password")
 
-@auth.route("/fallback")
-def fallback():
-  return render_template("general/fallback.html")
+    user = Users.query.filter_by(Email=email).first()
+
+    if user:
+      if password == user.Password:
+        login_user(user, remember=True)
+        print(user)
+        return redirect(url_for("general.index"))
+      else:
+        return redirect(url_for("auth.login"))
+
+  else:
+    return render_template("auth/login.html")
+
+@auth.route("/logout")
+def logout():
+  logout_user()
+  return redirect(url_for("general.index"))
